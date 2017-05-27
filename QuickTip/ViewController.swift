2a23be214b66b3a6032627ecfd3e660f99a65947
bfleury2017 <bfleury@outlook.com>
2017-05-27 12:12:29 -0400

    Initial Commit

---
+++ QuickTip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickTip
+//
+//  Created by Brian on 5/27/17.
+//  Copyright © 2017 Fleury Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
